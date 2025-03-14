8a4630c64074a619f3db6705911356ea2303dab0
orangeisland66 <orangeisland66@gmail.com>
2025-03-14 14:58:55 +0800

    软件系统实践第四次作业

---
+++ assignment4/Alarm/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+public class AlarmClock
+{
+    public delegate void TickEventHandler(object sender, EventArgs e);
+    public event TickEventHandler Tick;
+
+    public delegate void AlarmEventHandler(object sender, EventArgs e);
+    public event AlarmEventHandler Alarm;
+
+    private int alarmTime;
+    private int currentTime;
+
+    public AlarmClock(int alarmTime)
+    {
+        this.alarmTime = alarmTime;
+        this.currentTime = 0;
+    }
+
+    public void Start()
+    {
+        while (true)
+        {
+            OnTick();
+
+            if (currentTime == alarmTime)
+            {
+                OnAlarm();
+                break;
+            }
+
+            Thread.Sleep(1000);
+            currentTime++;
+        }
+    }
+
+    protected virtual void OnTick()
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+    }
+
+    protected virtual void OnAlarm()
+    {
+        Alarm?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        AlarmClock clock = new AlarmClock(5);
+
+        clock.Tick += (sender, e) =>
+        {
+            Console.WriteLine("嘀嗒...");
+        };
+
+        clock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("响铃！时间到了！");
+        };
+
+        clock.Start();
+    }
+}
---
+++ assignment4/List/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+
+            Console.WriteLine("链表元素:");
+            intlist.ForEach(x => Console.Write(x+" "));
+            Console.WriteLine();
+
+            int max = int.MinValue;
+            intlist.ForEach(x => max = Math.Max(max, x));
+            Console.WriteLine($"最大值: {max}");
+
+            int min = int.MaxValue;
+            intlist.ForEach(x => min = Math.Min(min, x));
+            Console.WriteLine($"最小值: {min}");
+
+            int sum = 0;
+            intlist.ForEach(x => sum += x);
+            Console.WriteLine($"求和: {sum}");
+
+
+            //GenericList<string> strList = new GenericList<string>();
+            //for (int x = 0; x < 10; x++)
+            //{
+            //    strList.Add("str" + x);
+            //}
+
+            //Console.WriteLine("\n字符串链表元素:");
+            //strList.ForEach(x => Console.WriteLine(x));
+
+
+
+        }
+
+    }
+}
